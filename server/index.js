5be5da5b7a69aa25e1576b37c12382154b707908
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-05-12 16:40:26 +0300

    get method handler added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import cors from 'cors'
-import { MongoClient } from 'mongodb'
-import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+const express = require('express')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const StatusCodes = require('http-status-codes').StatusCodes
+const ReasonPhrases = require('http-status-codes').ReasonPhrases
+const balanceEq = require('chem-eb')
 
 const PORT = 3000
 const MONGO_URI = 'mongodb+srv://user123:pass321@cluster0.tq8mvyq.mongodb.net/'
@@ -15,26 +17,42 @@ app.listen(PORT, () => console.log(`listening port ${PORT}`))
 
 const client = new MongoClient(MONGO_URI)
 
-client.connect().then(() => {
-  console.log('connected to database')
-})
+client
+  .connect()
+  .then(() => {
+    console.log('connected to database')
+  })
+  .catch((err) => console.log(err.message))
 
 const collection = client.db('course-project').collection('periodic-table')
+
+app.get('/', async (req, res) => {
+  try {
+    const elements = await collection.find().toArray()
+    if (elements.length > 0) {
+      return res.status(StatusCodes.OK).send(elements)
+    } else if (elements.lenght === 0) {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND)
+    }
+    //else
+    //   return res
+    //     .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    //     .send(ReasonPhrases.INTERNAL_SERVER_ERROR)
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message)
+  }
+})
 
 app.post('/', async (req, res) => {
   try {
     const elementName = req.body.symbol
     const element = await collection.findOne({ symbol: elementName })
     if (element) {
-      console.log(StatusCodes.OK)
       return res.status(StatusCodes.OK).send(element)
-    }
-    if (element === null) {
-      console.log(StatusCodes.NOT_FOUND)
+    } else if (element === null) {
       return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND)
-    } else throw new Error(ReasonPhrases.INTERNAL_SERVER_ERROR)
-  } catch (err) {
-    console.log(err.message)
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message)
+    }
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message)
   }
 })
