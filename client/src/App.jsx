342e177a8d23df27cdb53a6c4f00c22094f055eb
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-05-24 00:11:19 +0300

    fixed App structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import EquationForm from './components/EquationForm'
 import Table from './components/Table'
+import CaclulatorPanel from './components/CaclulatorPanel'
 
 function App() {
   return (
-    <>
+    <div className="App">
       <EquationForm />
+      <CaclulatorPanel />
       <Table />
-    </>
+    </div>
   )
 }
 
