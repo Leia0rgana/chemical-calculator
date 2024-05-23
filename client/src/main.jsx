a26101c31579f401f89f2a0d1bc78b4ba170e0d9
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-05-24 00:13:02 +0300

    App wrapped in router provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,26 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Menu from './components/UI/Menu'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 )
