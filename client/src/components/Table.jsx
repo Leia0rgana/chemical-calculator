9c65fccc57d2bc42e9277dd5f81e36f5f28dafd1
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-05-26 19:39:09 +0300

    elements location fixed

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,11 +1,22 @@
 import styles from './Table.module.css'
 import { useGetElementsQuery } from '../redux/elementsApi'
 import { useDispatch, useSelector } from 'react-redux'
+
 import {
   setEquation,
   toggleEqualizeBtn,
   selectIsActiveEqualizeBtn,
 } from '../redux/slices/equationSlice'
+
+let lanthanoidAndActinoid = []
+
+for (let i = 57; i <= 71; i += 1) {
+  lanthanoidAndActinoid.push(i)
+}
+
+for (let i = 89; i <= 103; i += 1) {
+  lanthanoidAndActinoid.push(i)
+}
 
 const Table = () => {
   const { data, isLoading, error } = useGetElementsQuery()
@@ -17,6 +28,19 @@ const Table = () => {
     !isActiveEqualizeBtn && dispatch(toggleEqualizeBtn())
   }
 
+  const getElementLocation = (number, period, group) => {
+    if (lanthanoidAndActinoid.includes(number)) {
+      return {
+        gridColumn: group + 1,
+        gridRow: period + 2,
+      }
+    } else
+      return {
+        gridColumn: group,
+        gridRow: period,
+      }
+  }
+
   return (
     <div className={styles.table}>
       {error ? (
@@ -25,16 +49,23 @@ const Table = () => {
         <h2>Loading...</h2>
       ) : (
         data &&
-        data.map((element) => (
-          <button
-            className={styles.element}
-            key={element.number}
-            onClick={() => handleClick(element.symbol)}
-          >
-            <div>{element.number}</div>
-            <div>{element.symbol}</div>
-          </button>
-        ))
+        data.map((element) => {
+          return (
+            <button
+              key={element.number}
+              className={styles.element}
+              style={getElementLocation(
+                element.number,
+                element.period,
+                element.group
+              )}
+              onClick={() => handleClick(element.symbol)}
+            >
+              <div>{element.number}</div>
+              <div>{element.symbol}</div>
+            </button>
+          )
+        })
       )}
     </div>
   )
